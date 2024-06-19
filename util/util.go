a07af4b3a16ae5cecd4553bac4d3cfb547eeb172
Primexz <johann@pepebot.info>
2024-06-20 01:00:40 +0200

    fix: prevent segfault when rpc connection fails

--- util/util.go
+++ util/util.go
@@ -1,5 +1,7 @@
 package util
 
+import "reflect"
+
 func BoolToFloat64(b bool) float64 {
 	if b {
 		return 1
@@ -11,6 +13,14 @@ func AnyNil(params ...interface{}) bool {
 	for _, param := range params {
 		if param == nil {
 			return true
+		}
+
+		v := reflect.ValueOf(param)
+		switch v.Kind() {
+		case reflect.Ptr, reflect.Map, reflect.Slice, reflect.Chan, reflect.Func:
+			if v.IsNil() {
+				return true
+			}
 		}
 	}
 	return false
